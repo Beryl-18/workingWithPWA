62680d4089498a1fb892d8a116c11ca146808884
Beryl-18 <59616439+Beryl-18@users.noreply.github.com>
2021-11-13 21:45:44 +0400

    Creating Lesson Objects

---
+++ lessons.js
@@ -0,0 +1,93 @@
+let lessons = [
+    {
+        "id" : '1',
+        "Subject" : 'Mathematics',
+        "Location" : 'Qusais',
+        "price" : 50.00,
+        "image" : 'catfood.jpg',
+        "spaces": 5,
+        "showProduct": true
+    },
+    {
+        "id" : '2',
+        "Subject" : 'Biology',
+        "Location" : 'Muhaisnah',
+        "price" : 50.00,
+        "image" : 'catfood.jpg',
+        "spaces": 5,
+        "showProduct": true
+    },
+    {
+        "id" : '3',
+        "Subject" : 'Chemistry',
+        "Location" : 'Qusais',
+        "price" : 60.00,
+        "image" : 'catfood.jpg',
+        "spaces": 5,
+        "showProduct": true
+    },
+    {
+        "id" : '4',
+        "Subject" : 'Geography',
+        "Location" : 'Qusais',
+        "price" : 40.00,
+        "image" : 'catfood.jpg',
+        "spaces": 5,
+        "showProduct": true
+    },
+    {
+        "id" : '5',
+        "Subject" : 'History',
+        "Location" : 'Bur Dubai',
+        "price" : 45.00,
+        "image" : 'catfood.jpg',
+        "spaces": 5,
+        "showProduct": true
+    },
+    {
+        "id" : '6',
+        "Subject" : 'Music',
+        "Location" : 'Knowledge Park',
+        "price" : 60.00,
+        "image" : 'catfood.jpg',
+        "spaces": 5,
+        "showProduct": true
+    },
+    {
+        "id" : '7',
+        "Subject" : 'Finance',
+        "Location" : 'Deira',
+        "price" : 40.00,
+        "image" : 'catfood.jpg',
+        "spaces": 5,
+        "showProduct": true
+    },
+    {
+        "id" : '8',
+        "Subject" : 'Stocks Trading',
+        "Location" : 'Jumeirah',
+        "price" : 70.00,
+        "image" : 'catfood.jpg',
+        "spaces": 5,
+        "showProduct": true
+    },
+    {
+        "id" : '9',
+        "Subject" : 'Culinary Arts',
+        "Location" : 'Qusais',
+        "price" : 60.00,
+        "image" : 'catfood.jpg',
+        "spaces": 5,
+        "showProduct": true
+    },
+    {
+        "id" : '10',
+        "Subject" : 'Physics',
+        "Location" : 'Al Barsha',
+        "price" : 60.00,
+        "image" : 'catfood.jpg',
+        "spaces": 5,
+        "showProduct": true
+    },
+
+]
