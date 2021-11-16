4848d2a4d7ddf79b9616fec97b265da409935c0b
Beryl-18 <beryla.s@hotmail.com>
2021-11-16 12:48:26 +0400

    recreated logic for add/remove to cart

--- lessons.js
+++ lessons.js
@@ -1,103 +1,113 @@
 let lessons = [
     {
-        "id" : '1',
+        "id" : 1,
         "Subject" : 'Mathematics',
         "Location" : 'Qusais',
         "price" : 200.00,
         "image" : 'catfood.jpg',
         "spaces": 5,
         "quantity":0,
-        "showLesson": true
+        "showLesson": true,
+        "displayed":false
     },
     {
-        "id" : '2',
+        "id" : 2,
         "Subject" : 'Biology',
         "Location" : 'Muhaisnah',
         "price" : 180.00,
         "image" : 'catfood.jpg',
         "spaces": 5,
         "quantity":0,
-        "showLesson": true
+        "showLesson": true,
+        "displayed":false
     },
     {
-        "id" : '3',
+        "id" : 3,
         "Subject" : 'Chemistry',
         "Location" : 'Qusais',
         "price" : 180.00,
         "image" : 'catfood.jpg',
         "spaces": 5,
         "quantity":0,
-        "showLesson": true
+        "showLesson": true,
+        "displayed":false
     },
     {
-        "id" : '4',
+        "id" : 4,
         "Subject" : 'Geography',
         "Location" : 'Qusais',
         "price" : 150.00,
         "image" : 'catfood.jpg',
         "spaces": 5,
         "quantity":0,
-        "showLesson": true
+        "showLesson": true,
+        "displayed":false
     },
     {
-        "id" : '5',
+        "id" : 5,
         "Subject" : 'History',
         "Location" : 'Bur Dubai',
         "price" : 150.00,
         "image" : 'catfood.jpg',
         "spaces": 5,
         "quantity":0,
-        "showLesson": true
+        "showLesson": true,
+        "displayed":false
     },
     {
-        "id" : '6',
+        "id" : 6,
         "Subject" : 'Music',
         "Location" : 'Knowledge Park',
         "price" : 420.00,
         "image" : 'catfood.jpg',
         "spaces": 5,
         "quantity":0,
-        "showLesson": true
+        "showLesson": true,
+        "displayed":false
     },
     {
-        "id" : '7',
+        "id" : 7,
         "Subject" : 'Finance',
         "Location" : 'Deira',
         "price" : 425.00,
         "image" : 'catfood.jpg',
         "spaces": 5,
         "quantity":0,
-        "showLesson": true
+        "showLesson": true,
+        "displayed":false
     },
     {
-        "id" : '8',
+        "id" : 8,
         "Subject" : 'Stocks Trading',
         "Location" : 'Jumeirah',
         "price" : 750.00,
         "image" : 'catfood.jpg',
         "spaces": 5,
         "quantity":0,
-        "showLesson": true
+        "showLesson": true,
+        "displayed":false
     },
     {
-        "id" : '9',
+        "id" : 9,
         "Subject" : 'Culinary Arts',
         "Location" : 'Qusais',
         "price" : 500.00,
         "image" : 'catfood.jpg',
         "spaces": 5,
         "quantity":0,
-        "showLesson": true
+        "showLesson": true,
+        "displayed":false
     },
     {
-        "id" : '10',
+        "id" : 10,
         "Subject" : 'Physics',
         "Location" : 'Al Barsha',
         "price" : 350.00,
         "image" : 'catfood.jpg',
         "spaces": 5,
         "quantity":0,
-        "showLesson": true
+        "showLesson": true,
+        "displayed":false
     },
 
 ]
